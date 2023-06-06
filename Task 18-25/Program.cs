8110781d8f8374096ce2300a85ec96ca2788acd2
KiselyovSaint <116631267+KiselyovSaint@users.noreply.github.com>
2023-06-06 13:46:42 +0300

    Add files via upload

---
+++ Task 18-25/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student("Артём", "Киселёв", "Александрович", 18);
+            Console.WriteLine("Студент: ");
+            Console.WriteLine("Имя: " + student1._FName);
+            Console.WriteLine("Фамилия: " + student1._LName);
+            Console.WriteLine("Отчество: " + student1._Patronymic);
+            Console.WriteLine("Возраст: " + student1._Age);
+        }
+    }
+}
---
+++ Task 18-25/Student.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Student
+    {
+        private string LName;
+        private string FName;
+        private string Patronymic;
+        private int Age;
+
+        public Student(string LName, string FName, string Patronymic, int Age)
+        {
+            this.LName = LName;
+            this.FName = FName;
+            this.Patronymic = Patronymic;
+            this.Age = Age;
+        }
+        public string _LName
+        {
+            get { return LName; }
+            set { LName = value; }
+        }
+        public string _FName
+        {
+            get { return FName; }
+            set { FName = value; }
+        }
+        public string _Patronymic
+        {
+            get { return Patronymic; }
+            set { Patronymic = value; }
+        }
+        public int _Age
+        {
+            get { return Age; }
+            set { Age = value; }
+        }
+
+    }
+}
